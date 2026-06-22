b39f0a6379fdfe16475a068a2ea7284b5caff179
abdullahsheikh67 <abdullahsheikh6883@gmail.com>
2026-06-22 14:36:08 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# a = "Abdullah"
+
+# b = ""
+
+# for i in range(len(a)-1, -1, -1):
+#     b += a[i]
+# print(b)
+
+
+a = "Abdullah"
+
+b = ""
+
+for i in range(len(a)-1, -1, -1):
+    b += a[i]
+
+if a==b:
+    print("Palindrome")
+
+else:    
+    print("Not Palindrome")
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+a = "Abdullah"
+
+b = ""
+
+for i in range(len(a)-1, 0, -1):
+    b += a[i]
+print(b)
